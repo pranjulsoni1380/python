cc1a13463d68bb3ac27773fea344757c8823068c
pranjulsoni1380 <38792462+pranjulsoni1380@users.noreply.github.com>
2021-06-30 21:02:15 +0530

    Update userlist.py

--- userlist.py
+++ userlist.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+
+
 import pymysql
+import sys
+
+args=
 
 con = pymysql.connect('database-1.cy1jk3ql5lqn.ap-south-1.rds.amazonaws.com', 'MYSQL_USERNAME',
     'MYSQL_PASSWORD')
