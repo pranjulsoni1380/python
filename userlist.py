04c9a1a8916d80d0536aac0570367191d7318b69
pranjulsoni1380 <38792462+pranjulsoni1380@users.noreply.github.com>
2021-06-30 10:46:09 +0530

    Create userlist.py

---
+++ userlist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+
+
+import pymysql
+
+con = pymysql.connect('localhost', 'user7',
+    's$cret', 'testdb')
+
+try:
+
+    with con.cursor() as cur:
+
+        cur.execute('SELECT user FROM mysql.user')
+
+finally:
+
+    con.close()
